87fc6ea37022c59b033b81225d507aa910aa6ab5
Nem <nemx14@gmail.com>
2019-02-13 16:10:16 -0500

    updated animation delay on page 'is-loading' in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@
 //			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
-				}, 1800);
+				}, 800);
 //			});
 
 		// Fix: Placeholder polyfill.
